02f444964802508093e32918eeed0d3962a17776
darwinsychangco <darwinsychangco@gmail.com>
2021-05-24 23:15:56 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+const element = document.getElementById("root");
+ReactDOM.render(<App />, element);
